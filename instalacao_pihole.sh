0b92e4fccb8aaaab38c108cba9ede2649a1a3009
Fernando Dias <ferpdias@gmail.com>
2026-08-17 16:58:43 -0300

    Instalação e configuração do Pi-Hole

--- instalacao_pihole.sh
+++ instalacao_pihole.sh
@@ -9,5 +9,22 @@
 ## 
 ##########
 
-# Atualização do SO
+# Configura as variáveis de ambiente
+source .env
+
+# Download do script de instalação  
+#wget -O basic-install.sh https://install.pi-hole.net
+
+# Copiar o arquivo pihole.toml
+mkdir -p /etc/pihole
+cp pihole.toml /etc/pihole/pihole.toml
+
+# Instalação do Pi-hole
+bash basic-install.sh --unattended
+
+# Atualizar senha
+pihole setpassword ${PASSWORD_PIHOLE}
+
+# Atualizar o Gravity
+pihole updateGravity
 
